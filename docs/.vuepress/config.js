9a1c89426d8bcf842b876e2b58aa47cf3bafed92
JuneJH <15310487388@163.com>
2021-12-19 02:47:57 +0800

    feat(*): 新增blog项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: '首页', link: '/' },
+        { text: 'vue相关', link: '/blog/1.html' },
+      ],
+      sidebar: 'auto'
+    }
+  }
