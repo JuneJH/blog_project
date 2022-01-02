3edbda1f31feda13e284742f421fc05e14732cfe
JuneJH <15310487388@163.com>
2022-01-02 12:58:59 +0800

    fix(*): 处理提交规范

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title:"JuneJH",
+    themeConfig: {
+      nav: [
+        { text: 'GitHub', link: 'https://github.com/JuneJH' },
+        // { text: '项目相关', link: '/' },
+      ],
+      sidebar: 'auto'
+    },
+    plugins: ['autobar']
+  }
